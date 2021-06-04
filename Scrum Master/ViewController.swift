f33111eec616d7c7710b48e4c15de9026e946f4f
danielsason112 <31284543+danielsason112@users.noreply.github.com>
2021-06-04 06:28:49 -0700

    Initial Commit

---
+++ Scrum Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scrum Master
+//
+//  Created by user196686 on 6/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Scrum MasterTests/Scrum_MasterTests.swift
@@ -0,0 +1,33 @@
+//
+//  Scrum_MasterTests.swift
+//  Scrum MasterTests
+//
+//  Created by user196686 on 6/4/21.
+//
+
+import XCTest
+@testable import Scrum_Master
+
+class Scrum_MasterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
